89fd3bf40e6a20d8a93494d9b099cce995840e45
Mohammed Sufyan <mohammed.sufyan@actonate.com>
2017-11-29 19:06:15 +0530

    initial commit.

---
+++ js/app.js
@@ -0,0 +1,49 @@
+(function() {
+    console.log("App JS loaded.");
+  
+    $(document).ready(function() {
+		console.log("Document Ready!");
+
+		particlesJS.load('particle', 'js/particlejs-config.json', function() {
+			console.log('callback - particles.js config loaded');
+		});
+  
+		var typed = new Typed('#anim-text', {
+			strings: [
+			  "Hello <strong style='color: #fff;'>World!</strong>",
+			  "I'm <strong style='color: #fff;'>Mohammed Sufyan Shaikh</strong>",
+			  "Software Developer from <strong style='color: #fff;'>Vadodara</strong>",
+			//   "I love to code, always excited to try new <strong style='color: #fff;'>programming languages.</strong>",
+			  "I believe in <strong style='color: #fff;'>'First solve the problem, Then write the code.'</strong>"
+			],
+			typeSpeed: 70,
+			backSpeed: 30,
+			loop: true,
+			smartBackspace: true,
+			cursorChar: '',
+			startDelay: 1000,
+			backDelay: 600,
+		});
+  
+		//OwlCarousel
+        $('.owl-carousel').owlCarousel({
+			// loop:true,
+			margin:10,
+			// nav:true,
+			lazyLoad:true,
+			stagePadding: 50,
+			dots: true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				1000:{
+					items:3
+				}
+			}
+		});
+    });
+})();
